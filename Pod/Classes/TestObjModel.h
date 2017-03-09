63ba2efa677b187d8ec01dac4f99f4b8bd2db9d0
jiaxuzhou <766032945@qq.com>
2017-03-09 16:38:20 +0800

    tt
    txt

---
+++ Pod/Classes/TestObjModel.h
@@ -0,0 +1,15 @@
+//
+//  TestObjModel.h
+//  Pods
+//
+//  Created by Dhgate on 17/3/9.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObjModel : NSObject
+
+- (void)testMethod;
+
+@end
---
+++ Pod/Classes/TestView.h
@@ -0,0 +1,15 @@
+//
+//  TestView.h
+//  Pods
+//
+//  Created by Dhgate on 17/3/9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+- (void)createViewWith:(NSString *)str;
+
+@end
